38efbe0ee5f49cae0be691ed9f7809594828cf61
hmtheboy154 <buingoc67@gmail.com>
2023-01-13 11:26:56 -0500

    Add /vendor/lib{64}/egl search path for non-treblized builds
    Required for ANGLE EGL/GLESv2 libraries
    
    Change-Id: I5e2439703bf333eb204bc6e9aab86b2690ce6dd1

--- contents/namespace/systemdefault.cc
+++ contents/namespace/systemdefault.cc
@@ -106,6 +106,8 @@ Namespace BuildSystemDefaultNamespace([[maybe_unused]] const Context& ctx) {
   }
   if (!is_fully_treblelized) {
     ns.AddSearchPath("/vendor/${LIB}");
+    ns.AddSearchPath("/system/vendor/${LIB}/egl");
+    ns.AddSearchPath("/vendor/${LIB}/egl");
     ns.AddSearchPath("/vendor/${LIB}/hw");
     ns.AddSearchPath("/odm/${LIB}");
   }
